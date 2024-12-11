324e343bbe4c0ce79c8346b0f223c1911a1d4315
Morcous <osa22228@gmail.com>
2024-12-11 20:37:10 +0200

    additional changes to structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'workshop-project';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { HighlightsBannerComponent } from './Components/highlights-banner/highlights-banner.component';
+import { ThingsWeDoComponent } from './Components/things-we-do/things-we-do.component';
+import { LatestNewsComponent } from './Components/latest-news/latest-news.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    HighlightsBannerComponent,
+    ThingsWeDoComponent,
+    LatestNewsComponent,
+    FooterComponent,
+    HeaderComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
