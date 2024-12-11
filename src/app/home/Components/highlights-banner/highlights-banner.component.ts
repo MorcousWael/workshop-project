710387857065f1c38edbc7be02e89231cbfa3165
Morcous <osa22228@gmail.com>
2024-12-11 19:25:34 +0200

    Project structure

---
+++ src/app/home/Components/highlights-banner/highlights-banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-highlights-banner',
+  imports: [],
+  templateUrl: './highlights-banner.component.html',
+  styleUrl: './highlights-banner.component.scss'
+})
+export class HighlightsBannerComponent {
+
+}
---
+++ src/app/home/Components/latest-news/latest-news.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-news',
+  imports: [],
+  templateUrl: './latest-news.component.html',
+  styleUrl: './latest-news.component.scss'
+})
+export class LatestNewsComponent {
+
+}
