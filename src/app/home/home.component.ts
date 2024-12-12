d376768998200c64709061b222daf6c6685b58f6
Morcous <osa22228@gmail.com>
2024-12-12 15:45:06 +0200

    Finished Header section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { HighlightsBannerComponent } from './Components/highlights-banner/highlights-banner.component';
 import { ThingsWeDoComponent } from './Components/things-we-do/things-we-do.component';
 import { LatestNewsComponent } from './Components/latest-news/latest-news.component';
-import { FooterComponent } from '../shared/footer/footer.component';
-import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +9,6 @@ import { HeaderComponent } from '../shared/header/header.component';
     HighlightsBannerComponent,
     ThingsWeDoComponent,
     LatestNewsComponent,
-    FooterComponent,
-    HeaderComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
