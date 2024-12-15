43358d02c611702f12125af340ff6bf473cd3b05
Morcous <osa22228@gmail.com>
2024-12-15 13:27:47 +0200

    implement news ,implement footer

--- src/app/home/Components/highlights-banner/highlights-banner.component.ts
+++ src/app/home/Components/highlights-banner/highlights-banner.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-highlights-banner',
-  imports: [],
+  imports: [CommonModule],
+  providers: [],
   templateUrl: './highlights-banner.component.html',
-  styleUrl: './highlights-banner.component.scss'
+  styleUrl: './highlights-banner.component.scss',
 })
 export class HighlightsBannerComponent {
-
+  // private http = inject(HttpClient);
+  // ngOnInit(): void {
+  //   this.fetchHighlights();
+  // }
+  // fetchHighlights(): void {
+  //   this.http
+  //     .get(' https://api.npoint.io/fee177346e7875554413 ')
+  //     .subscribe((data) => {
+  //       console.log(data);
+  //     });
+  // }
 }
--- src/app/home/Components/latest-news/latest-news.component.ts
+++ src/app/home/Components/latest-news/latest-news.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-latest-news',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './latest-news.component.html',
-  styleUrl: './latest-news.component.scss'
+  styleUrls: ['./latest-news.component.scss'],
 })
 export class LatestNewsComponent {
-
+  newsList = [
+    {
+      image: 'assets/news1.jpg',
+      title:
+        'Let us commit to do our part and advance the promise of equality, justice...',
+      date: 'Wed 12 Aug 2022',
+      category: 'Economics',
+      icon: 'calendar_today',
+    },
+    {
+      image: 'assets/news2.jpg',
+      title:
+        'In interactive live seminar, top lectures of the IHF present current topics...',
+      date: 'Wed 12 Aug 2022',
+      category: 'Sport',
+      icon: 'calendar_today',
+    },
+    {
+      image: 'assets/news3.jpg',
+      title: 'Libya to construct solar plant with Total Energies',
+      date: 'Wed 12 Aug 2022',
+      category: 'Technology',
+      icon: 'calendar_today',
+    },
+    {
+      image: 'assets/news4.jpg',
+      title: 'The 2nd IHF Wheelchair Handball Seminar begins this Saturday.',
+      date: 'Wed 12 Aug 2022',
+      category: 'Sport',
+      icon: 'calendar_today',
+    },
+    {
+      image: 'assets/news5.jpg',
+      title: 'President El Sisi Unveils “Egypt Vision 2030” Sustainable...',
+      date: 'Wed 12 Aug 2022',
+      category: 'Economics',
+      icon: 'calendar_today',
+    },
+    {
+      image: 'assets/news6.jpg',
+      title: 'COVID-19: get the latest updates, The MoF is leading work...',
+      date: 'Wed 12 Aug 2022',
+      category: 'Health',
+      icon: 'calendar_today',
+    },
+  ];
 }
